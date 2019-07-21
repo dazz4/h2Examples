5e61e42d309f913d13dc222358c273b08dae6b93
dazz4 <kaminski.darek@icloud.com>
2019-07-21 12:30:05 +0100

    Budget and Account repositories

---
+++ src/main/java/com/h2examples/repository/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.h2examples.repository;
+
+import com.h2examples.domain.Account;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface AccountRepository extends CrudRepository<Account, Long> { }
---
+++ src/main/java/com/h2examples/repository/BudgetRepository.java
@@ -0,0 +1,8 @@
+package com.h2examples.repository;
+
+import com.h2examples.domain.Budget;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public interface BudgetRepository extends CrudRepository<Budget, Long> { }
